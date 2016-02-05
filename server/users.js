af440712bb439539a4c21191499ae5d4a868d8bf
Rishat Muhametshin <rishatmuhametshin@gmail.com>
2016-02-05 15:40:32 +0400

    Added initial setup to populate users with a dummy user if there‘s none.

--- server/users.js
+++ server/users.js
@@ -1,3 +1,11 @@
 Meteor.publish("users", function () {
   return Meteor.users.find({}, {fields: {emails: 1, profile: 1}});
 });
+
+Meteor.startup(function () {
+  if ((Meteor.users.find().count() === 0) && Meteor.settings.initial) {
+    Accounts.createUser({
+      ...Meteor.settings.initial.user
+    });
+  }
+});
