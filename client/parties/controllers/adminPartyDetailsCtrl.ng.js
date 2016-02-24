b2d8c82211367fe723265dca3d7d4982120f6fb7
Rishat Muhametshin <rishatmuhametshin@gmail.com>
2016-02-24 17:24:22 +0400

    Brought back autobinding.

--- client/parties/controllers/adminPartyDetailsCtrl.ng.js
+++ client/parties/controllers/adminPartyDetailsCtrl.ng.js
@@ -1,23 +1,14 @@
 'use strict';
 angular.module('hof2').controller('adminPartyDetailsCtrl', ['$scope', '$stateParams', '$meteor', '$filter', '$rootScope', '$state', '$reactive', function ($scope, $stateParams, $meteor, $filter, $rootScope, $state, $reactive) {
+  $scope.users = $meteor.collection(Meteor.users, false).subscribe('users');
+  $scope.$meteorSubscribe('parties').then(() => {
+    $scope.party = $meteor.object(Parties, $stateParams.partyId);
+    $scope.partyImages = _.isObject($scope.party) ? $scope.party.images : [];
+  });
 
-	$reactive(this).attach($scope);
+  $reactive(this).attach($scope);
 
-	this.subscribe('users');
-	this.subscribe('parties', () => [], () => {
-		$scope.party = Parties.findOne({
-		  _id: $stateParams.partyId
-		});
-		$scope.partyImages = _.isObject($scope.party) ? $scope.party.images : [];
-		$scope.name = $scope.party.name;
-		$scope.description = $scope.party.description;
-
-		console.log('$scope.party', $scope.party);
-	});
-
-
-
-	this.subscribe('images', () => [this.getReactively('party')], () => {
+  this.subscribe('images', () => [this.getReactively('party')], () => {
     const partyImages = $scope.getReactively('partyImages');
 
     const partyImageIds = _.map(partyImages, ({id}) => id);
