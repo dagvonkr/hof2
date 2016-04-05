1a73f672ec9a8b5ac6fabb37ad65ff72af3748d6
Sebastian Sastre <sebastian@flowingconcept.com>
2016-04-05 13:01:31 -0300

    fixes the admin party detail controller

--- client/parties/controllers/adminPartyDetailsCtrl.ng.js
+++ client/parties/controllers/adminPartyDetailsCtrl.ng.js
@@ -1,6 +1,8 @@
 'use strict';
 angular.module('hof2').controller('adminPartyDetailsCtrl', ['$scope', '$stateParams', '$meteor', '$filter', '$rootScope', '$state', '$reactive', function ($scope, $stateParams, $meteor, $filter, $rootScope, $state, $reactive) {
   $scope.users = $meteor.collection(Meteor.users, false).subscribe('users');
+
+
   $scope.$meteorSubscribe('parties').then(() => {
     $scope.party = $meteor.object(Parties, $stateParams.partyId);
     $scope.partyImages = _.isObject($scope.party) ? $scope.party.images : [];
@@ -8,42 +10,13 @@ angular.module('hof2').controller('adminPartyDetailsCtrl', ['$scope', '$statePar
 
       });
 
-  $scope.$meteorSubscribe('images').then( () => {
-    $scope.images = $meteor.collection(() => {
-      return Images.find({
+  $scope.$meteorSubscribe('images').then(() => {
+    const theseImageIds = _.map($scope.party.images, image => image.id);
+    $scope.images = Images.find({
         _id: {
-          $in: _.map($scope.party.images, image => image.id)
+          $in: theseImageIds
         }
-      });
-    }).subscribe('images');
-    console.log('$scope.images detailsview', $scope.images)
+      }).fetch();
   });
+
 }]);
-
-// 	$reactive(this).attach($scope);
-
-// 	// this.subscribe('users');
-// 	this.subscribe('parties', () => [], () => {
-// 		$scope.party = Parties.findOne({
-// 		  _id: $stateParams.partyId
-// 		});
-// 		$scope.partyImages = _.isObject($scope.party) ? $scope.party.images : [];
-// 		$scope.name = $scope.party.name;
-// 		$scope.description = $scope.party.description;
-// 		$scope.editorcontent = $scope.party.editorcontent;
-
-// 		console.log('$scope.party', $scope.party);	
-// 		console.log('$scope.party.editorcontent', $scope.party.editorcontent);
-// 	});
-
-
-
-
-//   this.subscribe('images', () => [this.getReactively('party')], () => {
-//     const partyImages = $scope.getReactively('partyImages');
-
-//     const partyImageIds = _.map(partyImages, ({id}) => id);
-//     const images = _.filter(Images.find().fetch(), ({_id}) => _.contains(partyImageIds, _id));
-
-//     $scope.images = _.map(images, image => image.url());
-//     $scope.mainImageUrl = $scope.images[0]; // FIXME: the first image is the main one, right?
