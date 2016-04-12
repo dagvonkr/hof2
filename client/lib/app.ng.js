d392575cc9ba7c2821816ef8b7d971d6e96472bf
Sebastian Sastre <sebastian@flowingconcept.com>
2016-04-12 16:27:13 -0300

    branched from master and added what we wanted to see in staging, all works so far

--- client/lib/app.ng.js
+++ client/lib/app.ng.js
@@ -4,12 +4,25 @@ function onReady() {
   angular.bootstrap(document, ['hof2'], {
     strictDi: true
   });
+
+  initializeGA();
 }
 
 if (Meteor.isCordova)
   angular.element(document).on("deviceready", onReady);
 else
   angular.element(document).ready(onReady);
+
+
+function initializeGA () {
+  GARecordPage = function (pageLocation) {
+    ga('create', Meteor.settings.public.ga.account, 'auto');
+    ga('send', 'pageview', {
+    page: pageLocation
+  });
+  }
+}
+
 
 // elastic textarea
 // $(document).ready(function(){
@@ -18,8 +31,8 @@ else
 // });
 
 
-$(document).ready(function() {
-	$("#settings-button").draggable({
-	    handle: ".settings-modal"
-	});
-});
+// $(document).ready(function() {
+// 	$("#settings-button").draggable({
+// 	    handle: ".settings-modal"
+// 	});
+// });
