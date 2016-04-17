f9f73402e75c8af587b87c025e700d7e31affa6a
Sebastian Sastre <sebastian@flowingconcept.com>
2016-04-17 04:47:04 -0300

    removes dropzone

--- client/blazeUploader/blazeUploader.js
+++ client/blazeUploader/blazeUploader.js
@@ -1,10 +1,10 @@
 var tpl = Template.blazeUploader;
 
 tpl.onCreated(function () {
-  console.log('I am a blaze uploader that had IIIIIInstantiated');
+  // console.log('I am a blaze uploader that had IIIIIInstantiated');
 });
 
 
 tpl.onRendered(function () {
-  console.log('I am a blaze uploader that had RRRRRRendered');
+  // console.log('I am a blaze uploader that had RRRRRRendered');
 })
