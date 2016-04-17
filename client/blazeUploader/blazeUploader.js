d4e29590fa86c2e0d3d239964c7feea6b6cbba85
Sebastian Sastre <sebastian@flowingconcept.com>
2016-04-17 05:46:54 -0300

    handling the input change

--- client/blazeUploader/blazeUploader.js
+++ client/blazeUploader/blazeUploader.js
@@ -2,15 +2,20 @@ var tpl = Template.blazeUploader;
 
 tpl.onCreated(function () {
   // console.log('I am a blaze uploader that had IIIIIInstantiated');
+
 });
 
 
 tpl.onRendered(function () {
   // console.log('I am a blaze uploader that had RRRRRRendered');
+  var self = this;
+  $(self.find('input')).on('fileSelected', function (event, fileItem) {
+    console.log('fileSelected on input', fileItem);
+  });
 });
 
 tpl.events({
-  'click .progress-label': function (e, t) {
-    console.log('changed progress-label in', t, e);
-  }
+  // 'fileSelected input': function (e, t) {
+  //   console.log('fileSelected on input', t, e);
+  // }
 });
